2db8222e74ca5991e7fdc73801d231b8c17636d1
jimmywang54 <abc8755987@gmail.com>
2025-01-26 04:11:29 +0800

    feat: chat endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,18 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# Message Schema
+class ChatMessage(BaseModel):
+    message: str
+
 # Health check endpoint
 @app.get("/api/health")
 def health_check():
     return {"status": "ok"}
+
+# Chat endpoints
+@app.post("/api/chat")
+def chat(chat_message: ChatMessage):
+    resposne = "Hi there!"
+
+    return {"response": resposne}
